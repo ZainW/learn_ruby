67d4eb752886bb22648be6101d85411f7b4b3ccc
ZainW <zainthegreatcb@gmail.com>
2015-11-02 17:38:38 -0500

    finished pig latin and initial for silly blocks

---
+++ 05_silly_blocks/silly_blocks.rb
@@ -0,0 +1,16 @@
+def reverser
+	final_arr = []
+	yield.split(" ").each do |splitter|
+		final_arr.push(splitter.reverse)
+
+	end
+	final_arr.join()
+end
+def repeater(param)
+
+end
+
+
+def adder
+
+end
