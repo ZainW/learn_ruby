f1cf2cebceb57791642bbf98c9107b6cbab1bc53
ZainW <zainthegreatcb@gmail.com>
2015-11-02 15:33:57 -0500

    solved titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -11,6 +11,8 @@ def shoult
 
 end
 def titleize(str)
+	little_words = ["and", "or", "the", "over", "to", "the", "a", "but"]
+	str.split(' ').each_with_index.map {|word, index| little_words.include?(word) && index>0 ? word : word.capitalize }.join(' ')
 
 end
 def shout(str)
